f9842f216540106cf98cfd7041f7fdb58a093317
satsuki-yabu <tetecoo.1019@gmail.com>
2021-04-15 15:18:54 +0900

    データ保存の修正

--- event_manage_app_flutter/lib/Calender.dart
+++ event_manage_app_flutter/lib/Calender.dart
@@ -4,96 +4,93 @@ import 'package:flutter_calendar_carousel/classes/event.dart';
 import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart'
     show CalendarCarousel;
 import 'EventPlan.dart';
-import 'package:shared_preferences/shared_preferences.dart'; 
+import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert'; //JSON変換用
 
-class Calender extends StatefulWidget {  
-
-  Calender({Key key, this.title}):super(key: key);
+class Calender extends StatefulWidget {
+  Calender({Key key, this.title}) : super(key: key);
   final String title; //mainから渡されるタイトル
 
   @override
-
   _CalenderState createState() => _CalenderState();
-
 }
 
 class _CalenderState extends State<Calender> {
-
   //各日付と、日付に紐づけたイベントリスト。選択した日付をもとに、それぞれのイベントリストを抽出する。
-  //データ保存、データ読み込み、表示に使用する。
-  Map<DateTime,List<EventList>> allDataList; 
+
+  Map<DateTime, List<EventList>> allDataList; //データ保存、データ読み込み、表示に使用する。
 
   @override
   void initState() {
-   super.initState();
-
-   allDataList = {};
+    super.initState();
+    allDataList = {};
+    //起動時、データを読み込む
+    _readData();
   }
 
   //データ保存
   void _saveData() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance(); //prefs保存するデータを入れる
-    for( DateTime key in allDataList.keys){
-        String eventListJson = jsonEncode(allDataList[key]);   //Jsonにエンコード
-        await prefs.setString(key.toString(), eventListJson);
+    //prefsには保存するデータを入れる
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    // allDateListのkeyをもとに、valueを入れる
+    for (DateTime key in allDataList.keys) {
+      // String saveEventListJson = jsonEncode(allDataList[key]); //Jsonにエンコード
+      await prefs.setString(
+          key.toString(), jsonEncode(allDataList[key])); //key,valueのセットにして、保存する
     }
   }
 
   //データ読み込み
   void _readData() async {
-    for( DateTime key in allDataList.keys){ //allDataListに入っているValue（List<dynamic>）分、処理を繰り返す
+    // prefsには、読み込むデータを入れる
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    List<dynamic> readEventListJson = jsonDecode(prefs.getString(key.toString())); //Jsonからデコード //[{'eventTitle': 'aaa','eventValue':'bbb'}]
-      for(int i = 0; i < readEventListJson.length; i++){
-        //値を代入して、EventListクラスのインスタンスを作る
-        EventList readEventList = new EventList(readEventListJson[i]['eventTitle'],readEventListJson[i]['eventValue']);
-        // EventList sample[eventTitle] = readEventListJson[i]['eventValue'] ;
-        allDataList[key].add(readEventList);
-        // print('確認');
-        // print(allDataList[key]);
+    // prefs.getKeysで、全てのkeyを取得する
+    // keyを1つ取得し、valueを取得する
+
+    for (String key in prefs.getKeys()) {
+      //valueに何も入っていない場合、処理を実行しない
+      //Jsonからデコード //[{eventTitle: aaa,eventValue:bbb}]
+      //jsonDecode(prefs.getString(key));から取り出す時、続けて書くと読みづらいと思って、変数に入れた
+      List<dynamic> readEventListJson = jsonDecode(prefs.getString(key));
+      //allEventListに入れる、List<EventList>を作る
+      List<EventList> eventList = [];
+      for (int count = 0; count < readEventListJson.length; count++) {
+        // List<EventList>にいれるEventListインスタンスを作成する
+        EventList loadEventList = new EventList(
+            readEventListJson[count]["eventTitle"],
+            readEventListJson[count]["eventValue"]);
+        eventList.add(loadEventList);
       }
+      // keyを指定し、eventListを代入して、allEventListを作る
+      allDataList[DateTime.parse(key)] = eventList;
+      print(allDataList);
     }
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: <Widget>[
-          //データ保存
-          IconButton(
-            icon: Icon(Icons.upload_rounded),
-            onPressed: () => setState(() {
-              _saveData(); 
-            }),
-          ),
-          //データ読み込み
-          IconButton(
-            icon: Icon(Icons.download_rounded),
-            onPressed: () => setState(() {
-              _readData(); 
-            }),
-          ),
-        ]
-      ),
-      body: Container(
-        child: CalendarCarousel<Event>(
-          onDayPressed: (DateTime date, List<Event> events)async{
-            if(allDataList[date] == null){  //allDataListの中身がnullの場合、初期化する
+        // mainから渡されたtitle
+        appBar: AppBar(title: Text(widget.title)),
+        body: Container(child: CalendarCarousel<Event>(
+          onDayPressed: (DateTime date, List<Event> events) async {
+            if (allDataList[date] == null) {
+              //allDataListの中身がnullの場合、初期化する
               allDataList[date] = [];
             }
-              List<EventList> result = await Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => EventPlan(dataList: allDataList[date]), //dataListを、EventPlanに渡す
-                ),
-              );
-            allDataList[date] = result; //遷移先から戻ってきたとき、渡される値を入れる
+            // 遷移先から戻ってきたとき、List<EventList>を受け取ってresultに入れる
+            List<EventList> result = await Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => EventPlan(
+                    dataList: allDataList[date]), //dataListを、EventPlanに渡す
+              ),
+            );
+            //遷移先から戻ってきたとき、渡される値を入れる
+            allDataList[date] = result;
+            //遷移先から戻ってきたとき、データを保存する
+            _saveData();
           },
-        )
-      )
-    );
+        )));
   }
 }
